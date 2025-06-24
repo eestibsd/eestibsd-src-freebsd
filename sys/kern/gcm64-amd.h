81291db696ae6fc28a4d51af5fa8df7e930cf485
EestiBSD <ehiforgotmyname@gmail.com>
2025-06-25 02:56:05 +0300

    Update gcm64-amd.h

--- sys/kern/gcm64-amd.h
+++ sys/kern/gcm64-amd.h
@@ -1,3 +1,12 @@
 // Guitare Core Module 64 (amd64)
 // gcm64-amd.h (for gc64-amd.c)
-#define CACHE_SIZE NULL
+#define CACHE_SIZE 0
+#define BYTES 4026*1024
+
+#define AUDIO 76
+
+#define KERNEL_CONNECT 71
+#define KERNEL_RAWBYTE 70
+#define CHARACTERS     90
+
+#define xFAAAAAAAAAAAA 12
